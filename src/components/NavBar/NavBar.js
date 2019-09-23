82637fe084f5e1add5acd2150530d1defb82b5b1
charlescortez <charlesmph@gmail.com>
2019-09-23 10:29:52 -0400

    add modalContent state

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,49 +1,80 @@
 import React, { Component } from 'react';
 import './NavBar.css';
 import { Link } from 'react-router-dom';
+import ModalSplash from './ModalSplash';
+import Login from './Login';
+import SignUp from './Signup';
 
-class NavBar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
+class NavBar extends Component{
+
+    state = {
+        showModal: false,
+        modalContent: <ModalSplash />
     }
-    render() { 
-        return (
-        <div className="container-fluid nav">
-            <div className="row">
-                <nav className="transparent">
-                    <div className="nav-wrapper transparent">
-                        <Link to="/" className="left">
-                            <ul>
-                                <li>
-                                    <Link to ="/">airbnb</Link>
+
+    componentDidMount() {
+        this.setState({
+            modalContent: <ModalSplash changeModalContent={this.changeModalContent}/>
+        })
+    }
+
+    changeModalContent = (newContent) => {
+        let modalContent = <ModalSplash changeModalContent={this.changeModalContent}/>
+        if(newContent === 'login'){
+            modalContent = <Login changeModalContent={this.changeModalContent}/>
+        } else if (newContent === 'signup'){
+            modalContent = <SignUp changeModalContent={this.changeModalContent}/>
+        }
+        this.setState({
+            modalContent
+        })
+    }
+
+    signup = (e)=>{
+        document.querySelector('body').className = 'body-modal-show';
+        this.setState({
+            showModal: true
+        })
+    }
+    closeModal = (e)=>{
+        document.querySelector('body').className = '';
+        this.setState({
+            showModal: false
+        })
+    }
+
+    render(){
+        return(
+            <div className="container-fluid nav">
+                <div className="row">
+                    <nav className="transparent">
+                        <div className="nav-wrapper">
+                            <Link to="/" className="left"><img className="logo" alt="" src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"/></Link>
+                            <ul id="nav-mobile" className="right">
+                                <li><Link to="/host/homes">Host a Home</Link></li>
+                                <li><Link to="/host/experience">Host an experience</Link></li>
+                                <li><Link to="/help">Help</Link></li>
+                                <li className="nav-non-link" onClick={this.signup}>
+                                    Sign up
+                                </li>
+                                <li className="nav-non-link" onClick={this.signup}>
+                                    Log in
                                 </li>
                             </ul>
+                        </div>
+                    </nav>
+                </div>
 
-                            </Link>
-                        <ul id="nav-mobile" className="right">
-                            <li>
-                                <Link to="/host/homes">Homes</Link>
-                            </li>
-                            <li>
-                                <Link to="/host/experience">Experience</Link>
-                            </li>
-                            <li>
-                                <Link to="/help">Help</Link>
-                            </li>
-                            <li>
-                                <Link to="/sign-up">Sign Up</Link>
-                            </li>
-                            <li>
-                                <Link to="/log-in">Log In</Link>
-                            </li>
-                        </ul>
+                <div className="login-modal" style={this.state.showModal ? {"display": "block"} : {}} >
+                    <button id="close-modal" onClick={this.closeModal}>&Chi;</button>
+                    <div className="modal-content">
+                        {this.state.modalContent}
                     </div>
-                </nav>
+                </div>
             </div>
-        </div>
-        );
+
+        )
     }
 }
- 
+
 export default NavBar;
